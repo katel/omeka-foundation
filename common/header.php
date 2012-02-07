ee7722910bacad408cf1d86473f33974387f2954
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-02-07 17:18:38 -0500

    Adds the language info
    Calls get_html_lang() to put the language in the <head>

--- common/header.php
+++ common/header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php echo get_html_lang(); ?>">
 <head>
 <title><?php echo settings('site_title'); echo isset($title) ? ' | ' . $title : ''; ?></title>
 
