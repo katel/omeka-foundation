cfab876d0d1b02d28962fe2386bcc82b1f9a9760
Kate Lomax <kate.lomax@gmail.com>
2012-02-08 10:51:09 +0000

    Fixed some validation and formatting issues

--- common/footer.php
+++ common/footer.php
@@ -1,17 +1,13 @@
 
 <div class="row">
-<div id="footer" class="twelve columns">
- <?php admin_plugin_footer(); ?>
-  <ul class="pagination">
-
-   
-    <?php echo custom_public_nav_header(); ?></ul>
-   <p><a href="http://omeka.org" id="omeka-logo">Powered by Omeka</a> | <a href="http://omeka.org/codex/">Documentation</a> | <a href="http://omeka.org/forums/">Support Forums</a></p>
-     
-
+	<div id="footer" class="twelve columns">
+		<?php admin_plugin_footer(); ?>
+		<ul class="pagination">
+			<?php echo
+			custom_public_nav_header(); ?>
+		</ul>
+		<p>
+			<a href="http://omeka.org" id="omeka-logo" name="omeka-logo">Powered by Omeka</a> | <a href="http://omeka.org/codex/">Documentation</a> | <a href="http://omeka.org/forums/">Support Forums</a>
+		</p>
+	</div>
 </div>
-</div>
-</body>
-</html>
-
-
